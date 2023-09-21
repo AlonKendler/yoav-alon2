3318e30212f182ffec25c90db9cd54bb5a42b335
Alon <alonkendler@gmail.com>
2023-09-21 20:05:10 +0300

    remove all cors

--- server/index.js
+++ server/index.js
@@ -15,19 +15,14 @@ const io = require('socket.io')(http, {
   }
 });
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+
+
+app.use(cors());
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', routes);
 
 // Initialize codeSessions and codeBlockStatus maps
